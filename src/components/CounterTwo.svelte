76f88cf99e303449b1b6ceec144ed5d4d2e1632d
vxncetxn <thevancetan@gmail.com>
2022-01-27 20:50:08 +0800

    Initial commit

---
+++ src/components/CounterTwo.svelte
@@ -0,0 +1,34 @@
+<script>
+    let count = 0;
+  
+    function add() {
+      count += 2;
+    }
+  
+    function subtract() {
+      count -= 2;
+    }
+  </script>
+  
+  <div class="counter">
+    <button on:click={subtract}>-</button>
+    <pre>{ count }</pre>
+    <button on:click={add}>+</button>
+  </div>
+  <div class="message">
+    <slot />
+  </div>
+  
+  <style>
+    .counter{
+      display: grid;
+      font-size: 2em;
+      grid-template-columns: repeat(3, minmax(0, 1fr));
+      margin-top: 2em;
+      place-items: center;
+    }
+    .message {
+      text-align: center;
+    }
+  </style>
+  
